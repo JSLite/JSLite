0823979ec33a011d520e956ef9059c6eb2447e55
Learning2012 <398188662@qq.com>
2014-10-16 18:10:17 +0800

    $$.each通用例遍方法，可用于例遍对象和数组
    $$.each(['a', 'b', 'c'], function(index, item){
      console.log('item %d is: %s', index, item)
    })

--- JSLite.js
+++ JSLite.js
@@ -92,7 +92,18 @@
             }
             return "Object";
         },
-        error:function(msg) {throw msg;}
+        error:function(msg) {throw msg;},
+        each:function(elements, callback){
+			var i, key
+			if (this.likeArray(elements)) {
+				for (i = 0; i < elements.length; i++)
+					if (callback.call(elements[i], i, elements[i]) === false) return elements
+				} else {
+				for (key in elements)
+					if (callback.call(elements[key], key, elements[key]) === false) return elements
+			}
+			return elements
+		}
 	})
 
 	window.WCJ = window.$$ = WCJ;
