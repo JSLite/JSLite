f0ee95c556318c7dd4e13d06411095b453bd3b70
Learning2012 <398188662@qq.com>
2014-10-19 04:52:48 +0800

    Array.prototype.filter解决低版本浏览器对filter方法的支持

--- JSLite.js
+++ JSLite.js
@@ -175,6 +175,30 @@
 		    if (matchesSelector) return matchesSelector.call(element, selector);
 		}
 	});
+	
+	//解决低版本浏览器对filter方法的支持
+	if (!Array.prototype.filter){
+		Array.prototype.filter = function(fun /*, thisArg */){
+			"use strict";
+			if (this === void 0 || this === null)
+				throw new TypeError();
+			var t = Object(this);
+			var len = t.length >>> 0;
+			if (typeof fun !== "function")
+				throw new TypeError();
+			var res = [];
+			var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+			for (var i = 0; i < len; i++){
+				if (i in t){
+					var val = t[i];
+					if (fun.call(thisArg, val, i, t))
+					res.push(val);
+				}
+			}
+
+			return res;
+		};
+	}
 
 	//字符串处理
     WCJ.extend(String.prototype,{
