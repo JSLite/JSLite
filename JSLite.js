7ce8bb7251fc0ece688a9276235a371b31631d7e
jaywcjlove <398188662@qq.com>
2014-10-20 02:24:54 +0800

    fix:IE 对象不支持“forEach”属性或方法

--- JSLite.js
+++ JSLite.js
@@ -210,7 +210,6 @@
 					res.push(val);
 				}
 			}
-
 			return res;
 		};
 	}
@@ -225,6 +224,19 @@
 	        return -1;
 	    }
 	}
+	//低版本IE对forEach方法的支持
+	if (!Array.prototype.forEach) {  
+	    Array.prototype.forEach = function(fun /*, thisp*/){  
+	        var len = this.length;  
+	        if (typeof fun != "function")  
+	            throw new TypeError();  
+	        var thisp = arguments[1];  
+	        for (var i = 0; i < len; i++){  
+	            if (i in this)  
+	                fun.call(thisp, this[i], i, this);  
+	        }  
+	    };  
+	} 
 
 	//字符串处理
     WCJ.extend(String.prototype,{
