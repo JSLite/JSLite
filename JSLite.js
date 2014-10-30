f088a3205681f3063b00178664334896863e69f4
Learning2012 <398188662@qq.com>
2014-10-30 14:59:12 +0800

    $("<div>ss</div>"):生成节点
    $("<div>22222<div>233232</div></div><div>233232</div>")
    //⇒array

--- JSLite.js
+++ JSLite.js
@@ -10,6 +10,11 @@
 				var dom ;
     			if (!selector) 
     				dom = emptyArray,dom.selector = selector || '',dom.__proto__ = WCJ.fn.init.prototype;
+    			else if (typeof selector == 'string'){
+    				selector = selector.trim()
+    				if (selector[0] == '<' && /^\s*<(\w+|!)[^>]*>/.test(selector)) 
+        				dom = P.fragment(selector)
+    			}
     			else if (WCJ.isFunction(selector)) return WCJ(document).ready(selector)
     			else {
     				if (WCJ.isArray(selector))
