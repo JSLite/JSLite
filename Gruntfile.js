ddf6c724d754bfafab421c9e30660e47cac2f9ed
jaywcjlove <398188662@qq.com>
2015-03-23 03:00:27 +0800

    updata package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
         // watch插件的配置信息
         watch: {
             another: {
-                files: ['src/*.js'],
+                files: ['src/*.js','build/*.js'],
                 tasks: ['uglify'],
                 options: {
                     // Start another live reload server on port 1337
