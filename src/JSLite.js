dcf37d55454bcd6d3b82de450438be81e0336d7e
jaywcjlove <398188662@qq.com>
2015-03-22 13:29:22 +0800

    修复优化hasClass导致的bug

--- src/JSLite.js
+++ src/JSLite.js
@@ -195,8 +195,8 @@
 		hasClass:function(name){
 			if (!name) return false
 			return emptyArray.some.call(this, function(el){
-				return !((' ' + el.className + ' ').search(' ' + name + ' ') < 0);
-			});
+				return this.test(el.className)
+			}, new RegExp('(^|\\s)' + name + '(\\s|$)'));
 		},
 		addClass:function(name){
 			if (!name) return this;
