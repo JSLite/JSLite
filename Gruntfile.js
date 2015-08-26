61b3338e61575085ae926fd8fbf1657a5b527365
yanhaijing <yanhaijing@yeah.net>
2015-08-26 23:07:13 +0800

    改进构建流程 :dog:
    1. 清楚-合并-压缩
    2. 添加未压缩文件

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,42 +3,54 @@ module.exports = function(grunt) {
     // 任务配置,所有插件的配置信息
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            js: {
+                files: [{
+                    src: ['build/*']
+                }]
+            }
+        },
+        concat: {
+            options: {
+                banner: '/*! http://JSLite.io - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            js: {
+                src: [
+                    'src/polyfill.js',
+                    'src/JSLite.js',
+                    'src/form.js',
+                    'src/event.js',
+                    'src/ajax.js'
+                ],
+                dest: 'build/JSLite.js'
+            }
+        },
         // uglify插件的配置信息
         uglify: {
             options: {
-                // banner: '/*! http://JSLite.io - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
-                // beautify: true,
-                // mangle: false, //不混淆变量名
-                // compress:true,//打开或关闭使用默认选项源压缩。
+                preserveComments: 'some'
             },
-            app_task: {
-                files: {
-                    'build/JSLite.min.js': [
-                        'src/polyfill.js', 
-                        'src/JSLite.js', 
-                        'src/form.js', 
-                        'src/event.js', 
-                        'src/ajax.js'
-                    ]
-                }
+            js: {
+                src: '<%= concat.js.dest %>',
+                dest: 'build/JSLite.min.js'
             }
         },
         // watch插件的配置信息
         watch: {
-            another: {
+            js: {
                 files: ['src/*.js'],
-                tasks: ['uglify'],
-                options: {
-                    // Start another live reload server on port 1337
-                    livereload: 1337
-                }
+                tasks: ['default']
             }
         }
     });
-    // 告诉grunt我们将使用插件
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 任务加载
+    require('load-grunt-tasks')(grunt, {
+        scope: 'devDependencies'
+    });
+
     // 告诉grunt当我们在终端中输入grunt时需要做些什么
-    grunt.registerTask('default', ['uglify','watch']);
- 
+    grunt.registerTask('default', ['clean', 'concat', 'uglify']);
+
 };
