997ad06c7019110e70c58bbb3d21df0c586c8e73
jaywcjlove <398188662@qq.com>
2015-10-23 17:58:26 +0800

    更新jsdom 7.0.2

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,8 @@
+var Path = require('path')
+/**
+ * https://github.com/rstacruz/mocha-jsdom
+ */
+
 /*
  * store original global keys
  */
@@ -5,15 +10,28 @@
 var blacklist = Object.keys(global)
 blacklist.push('constructor')
 
+/*
+ * default config
+ */
 
 var defaults = {
-    globalize: true,
-    console: true,
-    useEach: false,
-    html: '<!doctype html><html><head><meta charset="utf-8"></head><body></body></html>'
+  globalize: true,
+  console: true,
+  useEach: false,
+  skipWindowCheck: true,
+  html: '<!doctype html><html><head><meta charset="utf-8"></head><body></body></html>'
 }
 
-var extend =function(des, src, override){
+/*
+ * simple jsdom integration.
+ * You can pass jsdom options in, too:
+ *
+ *     require('./support/jsdom')({
+ *       src: [ jquery ]
+ *     })
+ */
+
+function extend(des, src, override){
     if(src instanceof Array){
         for(var i = 0, len = src.length; i < len; i++)
         extend(des, src[i], override);
@@ -26,61 +44,110 @@ var extend =function(des, src, override){
     return des;
 }
 
+module.exports = function (_options) {
+  var options = extend(extend({}, defaults), _options)
 
-module.exports = function(_options){
+  var keys = []
 
-    var options = extend(extend({}, defaults), _options)
-    var before = options.useEach ? global.beforeEach : global.before
-    var keys = []
+  var before = options.useEach ? global.beforeEach : global.before
+  var after = options.useEach ? global.afterEach : global.after
 
+  /*
+   * register jsdom before the entire test suite
+   */
 
-    global.expect = require('chai').expect
+  global.expect = require('chai').expect
 
-
-    before(function (next) {
-
-        require('jsdom').env(extend(extend({}, options), { done: done }))
-
-        function done (errors, window) {
-            if (options.globalize) {
-                propagateToGlobal(window)
-            } else {
-                global.window = window
-            }
-
-            if (options.console) {
-                window.console = global.console
-            }
-
-            if (errors) {
-                return next(getError(errors))
-            }
-
-            next(null)
-        }
-    })
-
-
-
-    /*
-    * propagate keys from `window` to `global`
-    */
-
-    function propagateToGlobal (window) {
-        for (var key in window) {
-            if (!window.hasOwnProperty(key)) continue
-            if (~blacklist.indexOf(key)) continue
-            if (key in global) {
-                if (process.env.JSDOM_VERBOSE) {
-                    console.warn("[jsdom] Warning: skipping cleanup of global['" + key + "']")
-                }
-                continue
-            }
-
-            keys.push(key)
-            global[key] = window[key]
-        }
+  before(function (next) {
+    if (global.window && !options.skipWindowCheck) {
+      throw new Error(
+        'already a browser environment ' +
+        "twice. use 'skipWindowCheck' to disable this check.")
     }
+
+    require('jsdom').env(
+      extend(extend({}, options), { done: done }))
+
+    function done (errors, window) {
+      if (options.globalize) {
+        propagateToGlobal(window)
+      } else {
+        global.window = window
+      }
+
+      if (options.console) {
+        window.console = global.console
+      }
+
+      if (errors) {
+        return next(getError(errors))
+      }
+
+      next(null)
+    }
+  })
+
+  /*
+   * undo keys from being propagated to global after the test suite
+   */
+
+  after(function () {
+    if (options.globalize) {
+      keys.forEach(function (key) {
+        delete global[key]
+      })
+    } else {
+      delete global.window
+    }
+  })
+
+  /*
+   * propagate keys from `window` to `global`
+   */
+
+  function propagateToGlobal (window) {
+    for (var key in window) {
+      if (!window.hasOwnProperty(key)) continue
+      if (~blacklist.indexOf(key)) continue
+      if (key in global) {
+        if (process.env.JSDOM_VERBOSE) {
+          console.warn("[jsdom] Warning: skipping cleanup of global['" + key + "']")
+        }
+        continue
+      }
+
+      keys.push(key)
+      global[key] = window[key]
+    }
+  }
+
+  /*
+   * re-throws jsdom errors
+   */
+
+  function getError (errors) {
+    var data = errors[0].data
+    var err = data.error
+    err.message = err.message + ' [jsdom]'
+
+    // clean up stack trace
+    if (err.stack) {
+      err.stack = err.stack.split('\n')
+      .reduce(function (list, line) {
+        if (line.match(/node_modules.+(jsdom|mocha)/)) {
+          return list
+        }
+
+        line = line
+          .replace(/file:\/\/.*<script>/g, '<script>')
+          .replace(/:undefined:undefined/g, '')
+        list.push(line)
+        return list
+      }, []).join('\n')
+    }
+
+    return err
+  }
 }
 
 module.exports.rerequire = rerequire
@@ -89,25 +156,26 @@ module.exports.rerequire = rerequire
  * Requires a module via `require()`, but invalidates the cache so it may be
  * called again in the future. Useful for `mocha --watch`.
  *
- *     var rerequire = require('setup').rerequire
- *     var $ = rerequire('JSLite')
+ *     var rerequire = require('mocha-jsdom').rerequire
+ *     var $ = rerequire('jquery')
  */
 
 function rerequire (module) {
-    if (module[0] === '.') {
-        module = Path.join(Path.dirname(getCaller()), module)
-    }
+  if (module[0] === '.') {
+    module = Path.join(Path.dirname(getCaller()), module)
+  }
 
-    var oldkeys = Object.keys(require.cache)
-    var result = require(module)
-    var newkeys = Object.keys(require.cache)
-    newkeys.forEach(function (newkey) {
-        if (!~oldkeys.indexOf(newkey)) {
-            delete require.cache[newkey]
-        }
-    })
-    return result
+  var oldkeys = Object.keys(require.cache)
+  var result = require(module)
+  var newkeys = Object.keys(require.cache)
+  newkeys.forEach(function (newkey) {
+    if (!~oldkeys.indexOf(newkey)) {
+      delete require.cache[newkey]
+    }
+  })
+  return result
 }
+
 /**
  * Internal: gets the filename of the caller function. The `offset` defines how
  * many hops away it's expected to be from in the stack trace.
@@ -116,12 +184,12 @@ function rerequire (module) {
  */
 
 function getCaller (offset) {
-    /* eslint-disable handle-callback-err */
-    if (typeof offset !== 'number') offset = 1
-    var old = Error.prepareStackTrace
-    var err = new Error()
-    Error.prepareStackTrace = function (err, stack) { return stack }
-    var fname = err.stack[1 + offset].getFileName()
-    Error.prepareStackTrace = old
-    return fname
+  /* eslint-disable handle-callback-err */
+  if (typeof offset !== 'number') offset = 1
+  var old = Error.prepareStackTrace
+  var err = new Error()
+  Error.prepareStackTrace = function (err, stack) { return stack }
+  var fname = err.stack[1 + offset].getFileName()
+  Error.prepareStackTrace = old
+  return fname
 }
