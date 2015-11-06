7c10cbd3d85abfaac8cb6c9f712a77d75bf61f75
jaywcjlove <398188662@qq.com>
2015-11-07 01:17:32 +0800

    添加$.noConflict单元测试。

--- test/unit/core.js
+++ test/unit/core.js
@@ -1,9 +1,4 @@
 describe('core ', function () {
-
-    it('$.noConflict() - 放弃 JSLite 控制的$ 变量。', function () {
-
-
-    })
     
     it('$.trim() - 去掉字符串起始和结尾的空格。', function () {
         var str = '  JSLite官网  '
@@ -34,5 +29,22 @@ describe('core ', function () {
         assert.equal($('#test div').length, 1);
     })
 
+
+    it('$.noConflict() - 放弃 JSLite 控制的$ 变量。', function () {
+
+        var elm = document.getElementById("test");
+        elm.innerHTML = '<div class="jslite">Goodbye</div>';
+
+        JSLite.noConflict();
+        JSLite(document).ready(function($) {
+            assert.equal($('#test div').length, 1);
+            assert.ok($, '$对象不存在');
+        });
+        assert.notOk($, '$对象不存在');
+        var $ = JSLite.noConflict();
+        assert.ok($, '$对象不存在');
+        elm.innerHTML = '';
+
+    })
 
 })
