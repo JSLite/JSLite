2e6976f844df8ed17bb606208bca812fb0722627
jaywcjlove <398188662@qq.com>
2015-11-07 00:01:15 +0800

    添加方法$.trim()和.length测试

--- test/unit/core.js
+++ test/unit/core.js
@@ -6,8 +6,8 @@ describe('core ', function () {
     })
     
     it('$.trim() - 去掉字符串起始和结尾的空格。', function () {
-
-
+        var str = '  JSLite官网  '
+        expect($.trim(str)).to.equal('JSLite官网');
     })
     
     it('$.grep() - 使用过滤函数过滤数组元素。', function () {
@@ -22,8 +22,9 @@ describe('core ', function () {
 
     
     it('.length - 对象中元素的个数。', function () {
-
-
+        var elm = document.getElementById("test");
+        elm.innerHTML = '<div class="jslite">Goodbye</div>';
+        assert.equal($('#test div').length, 1);
     })
 
 
