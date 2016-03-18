d99695aa6c6bf000f4c78ebe2fbdff3cd39ca40d
jaywcjlove <398188662@qq.com>
2016-03-18 16:53:12 +0800

    删除无用的代码

--- rollup.js
+++ rollup.js
@@ -33,45 +33,15 @@ rollup({
 }).then( bundle => {
     bundle.write({ 
         dest: 'dist/JSLite.js', 
+        // output format - 'amd', 'cjs', 'es6', 'iife', 'umd'
+        // amd – 使用像requirejs一样的银木块定义
+        // cjs – CommonJS，适用于node和browserify / Webpack
+        // es6 (default) – 保持ES6的格式
+        // iife – 使用于<script> 标签引用的方式
+        // umd – 适用于CommonJs和AMD风格通用模式
         format: 'umd',
         banner: banner_str
     })   
 } ).catch(function(err){
     console.log(err);
 });
-
-
-    // return rollup({
-    //     entry: 'src/JSLite.js',
-    //     plugins: [
-    //         babel({
-    //             exclude: 'node_modules/**/*',
-    //             // //externalHelpers 不包含到我打包的JS里面去
-    //             // externalHelpers: true,
-    //         }),
-    //         uglify(),
-    //         replace({
-    //             delimiters: [ '<@','@>' ],
-    //             values: {
-    //                 VERSION: pkg.version
-    //             }
-    //         })
-    //     ]
-    // }).then( (bundle) => {
-    //     // 输出 bundle + sourcemap
-    //     var result = bundle.generate({
-    //         // output format - 'amd', 'cjs', 'es6', 'iife', 'umd'
-    //         // amd – 使用像requirejs一样的银木块定义
-    //         // cjs – CommonJS，适用于node和browserify / Webpack
-    //         // es6 (default) – 保持ES6的格式
-    //         // iife – 使用于<script> 标签引用的方式
-    //         // umd – 适用于CommonJs和AMD风格通用模式
-    //         format: 'umd',
-    //         moduleName:"JSLite",
-    //         // useStrict: true,
-    //         banner: banner_min_str
-    //     });
-        
-    //     fs.writeFileSync( 'dist/JSLite.min.js', banner_min_str + result.code );
-
-    // });
